de5311799f064a05df49b646962dda82df13c833
MichalNowak98 <48417024+MichalNowak98@users.noreply.github.com>
2019-05-11 00:58:37 +0200

    Added funtions to change RGB values of light. Added Toggle_Lamp() method turning lamp on and off. Added three pointers to trigger volumes.

--- LampAndObject/Source/LampAndObject/Lamp.cpp
+++ LampAndObject/Source/LampAndObject/Lamp.cpp
@@ -1,29 +1,125 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Lamp.h"
 
-// Sets default values
 ALamp::ALamp() : LightSource(CreateDefaultSubobject<UPointLightComponent>(TEXT("LightSource")))
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	//if creating of UPointLightComponent was successful
 	if (LightSource)
 	{
 		LightSource->SetIntensity(5000);
 	}
 }
 
-// Called when the game starts or when spawned
 void ALamp::BeginPlay()
 {
 	Super::BeginPlay();
 	Player = GetWorld()->GetFirstPlayerController()->GetPawn();
+	
 }
 
-// Called every frame
 void ALamp::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if (TriggerVolumeRed && TriggerVolumeGreen && TriggerVolumeBlue &&
+		!TriggerVolumeRed->IsOverlappingActor(Player) && !TriggerVolumeGreen->IsOverlappingActor(Player) && !TriggerVolumeBlue->IsOverlappingActor(Player))
+	{
+		Color_on(White);
+	}
+	else
+	{
+		if (TriggerVolumeRed && TriggerVolumeRed->IsOverlappingActor(Player))
+		{
+			Color_on(Red);
+		}
+		else
+		{
+			Color_off(Red);
+		}
+		if (TriggerVolumeGreen && TriggerVolumeGreen->IsOverlappingActor(Player))
+		{
+			Color_on(Green);
+		}
+		else
+		{
+			Color_off(Green);
+		}
+		if (TriggerVolumeBlue && TriggerVolumeBlue->IsOverlappingActor(Player))
+		{
+			Color_on(Blue);
+		}
+		else
+		{
+			Color_off(Blue);
+		}
+	}
+}
 
+void ALamp::Toggle_Lamp()
+{
+	if(LightSource->Intensity == 0)
+		LightSource->SetIntensity(5000);
+	else
+		LightSource->SetIntensity(0);
+}
+void ALamp::Color_on(EColor Color)
+{
+	FLinearColor color = LightSource->GetLightColor();
+	if (Player)
+	{
+		switch (Color)
+		{
+		case White:
+		{
+			color.R = color.G = color.B = 1;
+			break;
+		}
+		case Red:
+		{
+			color.R = 1;
+			break;
+		}
+		case Green:
+		{
+			color.G = 1;
+			break;
+		}
+		case Blue:
+		{
+			color.B = 1;
+			break;
+		}
+		default: {}
+		}
+		LightSource->SetLightColor(color, true);
+	}	
+}
+
+void ALamp::Color_off(EColor Color)
+{
+	FLinearColor color = LightSource->GetLightColor();
+	if (Player)
+	{
+		switch (Color)
+		{
+		case Red:
+		{
+			color.R = 0;
+			break;
+		}
+		case Green:
+		{
+			color.G = 0;
+			break;
+		}
+		case Blue:
+		{
+			color.B = 0;
+			break;
+		}
+		default: {}
+		}
+		LightSource->SetLightColor(color, true);
+	}
 }
 
--- LampAndObject/Source/LampAndObject/Lamp.h
+++ LampAndObject/Source/LampAndObject/Lamp.h
@@ -1,34 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
+
 #include "CoreMinimal.h"
 #include "Runtime/Engine/Classes/Components/PointLightComponent.h"
 #include "Engine/TriggerVolume.h"
+#include "Runtime/Core/Public/Math/Color.h"
 #include "GameFramework/Actor.h"
 #include "Lamp.generated.h"
-
-enum EColor { Red, Green, Blue };
 
 UCLASS()
 class LAMPANDOBJECT_API ALamp : public AActor
 {
 	GENERATED_BODY()
-
-public:
-	// Sets default values for this actor's properties
+	
+public:	
 	ALamp();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	//turns lamp on and off
+	void Toggle_Lamp();
+
+	enum EColor { White, Red, Green, Blue };
+protected:
+	virtual void BeginPlay() override;
 
 private:
 	UPROPERTY()
 		class UPointLightComponent *LightSource;
 	AActor *Player;
 
+	//pointers to trigger volumes from level
 	UPROPERTY(EditAnywhere)
-		ATriggerVolume *PressurePlate;
+		ATriggerVolume *TriggerVolumeRed;
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume *TriggerVolumeGreen;
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume *TriggerVolumeBlue;
+
+	//sets values of given channels to 1 keeping other values unchanged (mixed colors possible).
+	//R = G = B = 1 for "White"
+	void Color_on(EColor Color);
+
+	//sets values of given channels to 0
+	void Color_off(EColor Color);
 };
