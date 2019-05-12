d29c6417167f6cfbe709462068a160730b0c3ce3
MichalNowak98 <48417024+MichalNowak98@users.noreply.github.com>
2019-05-12 17:44:10 +0200

    Added basic replication prototype. TODO: game state with replication

--- LampAndObject/Source/LampAndObject/Lamp.cpp
+++ LampAndObject/Source/LampAndObject/Lamp.cpp
@@ -1,125 +1,281 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "Lamp.h"
 
-ALamp::ALamp() : LightSource(CreateDefaultSubobject<UPointLightComponent>(TEXT("LightSource")))
+
+ALamp::ALamp(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	CountRed = 0;
+	CountGreen = 0;
+	CountBlue = 0;
+	TriggerVolumeRedPushed = false;
+	TriggerVolumeGreenPushed = false;
+	TriggerVolumeBluePushed = false;
+
+	LightSource = CreateDefaultSubobject<UPointLightComponent>(TEXT("LightSource"));
+	SetReplicates(true);
+	bReplicates = true;
+	bNetLoadOnClient = true;
+	bAlwaysRelevant = true;
 
 	//if creating of UPointLightComponent was successful
 	if (LightSource)
 	{
 		LightSource->SetIntensity(5000);
+		color = LightSource->GetLightColor();
 	}
+}
+
+void ALamp::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ALamp, color);
+	DOREPLIFETIME(ALamp, CountRed);
+	DOREPLIFETIME(ALamp, CountGreen);
+	DOREPLIFETIME(ALamp, CountBlue);
+}
+
+void ALamp::SetLightColor_Implementation(const FLinearColor Color)
+{
+	//color = Color;
+	if (HasAuthority())
+	{
+		if (color.R == 1)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ServerRED"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ServerNoRED"));
+		}
+		if (color.B == 1)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ServerBlue"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ServerNoBlue"));
+		}
+		if (color.G == 1)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ServerGREEN"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ServerNoGREEN"));
+		}
+	}
+	else
+	{
+		if (color.R == 1)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ClientRED"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ClientNoRED"));
+		}
+		if (color.B == 1)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ClientBlue"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ClientNoBlue"));
+		}
+		if (color.G == 1)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ClientGREEN"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ClientNoGREEN"));
+		}
+	}
+	LightSource->SetLightColor(color, true);
+}
+
+void ALamp::increment_CountRed_Implementation()
+{
+	CountRed++;
+	UE_LOG(LogTemp, Warning, TEXT("Client cos robi!"));
+}
+
+void ALamp::increment_CountGreen_Implementation()
+{
+	CountGreen++;
+}
+
+void ALamp::increment_CountBlue_Implementation()
+{
+	CountBlue++;
+}
+
+void ALamp::decrement_CountRed_Implementation()
+{
+	CountRed--;
+}
+
+void ALamp::decrement_CountGreen_Implementation()
+{
+	CountGreen--;
+}
+
+void ALamp::decrement_CountBlue_Implementation()
+{
+	CountBlue--;
 }
 
 void ALamp::BeginPlay()
 {
 	Super::BeginPlay();
 	Player = GetWorld()->GetFirstPlayerController()->GetPawn();
-	
 }
 
 void ALamp::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	if (TriggerVolumeRed && TriggerVolumeGreen && TriggerVolumeBlue &&
-		!TriggerVolumeRed->IsOverlappingActor(Player) && !TriggerVolumeGreen->IsOverlappingActor(Player) && !TriggerVolumeBlue->IsOverlappingActor(Player))
+
+	if (TriggerVolumeRed && TriggerVolumeRed->IsOverlappingActor(Player))
 	{
-		Color_on(White);
+		if (!TriggerVolumeRedPushed)
+			increment_CountRed();
+		TriggerVolumeRedPushed = true;
 	}
 	else
 	{
-		if (TriggerVolumeRed && TriggerVolumeRed->IsOverlappingActor(Player))
-		{
-			Color_on(Red);
-		}
-		else
-		{
-			Color_off(Red);
-		}
-		if (TriggerVolumeGreen && TriggerVolumeGreen->IsOverlappingActor(Player))
-		{
-			Color_on(Green);
-		}
-		else
-		{
-			Color_off(Green);
-		}
-		if (TriggerVolumeBlue && TriggerVolumeBlue->IsOverlappingActor(Player))
-		{
-			Color_on(Blue);
-		}
-		else
-		{
-			Color_off(Blue);
-		}
+		if (TriggerVolumeRedPushed)
+			decrement_CountRed();
+		TriggerVolumeRedPushed = false;
 	}
+	if (TriggerVolumeGreen && TriggerVolumeGreen->IsOverlappingActor(Player))
+	{
+		if (!TriggerVolumeGreenPushed)
+			increment_CountGreen();
+		TriggerVolumeGreenPushed = true;
+	}
+	else
+	{
+		if (TriggerVolumeGreenPushed)
+			decrement_CountGreen();
+		TriggerVolumeGreenPushed = false;
+	}
+	if (TriggerVolumeBlue && TriggerVolumeBlue->IsOverlappingActor(Player))
+	{
+		if (!TriggerVolumeBluePushed)
+			increment_CountBlue();
+		TriggerVolumeBluePushed = true;
+	}
+	else
+	{
+		if (TriggerVolumeBluePushed)
+			decrement_CountBlue();
+		TriggerVolumeBluePushed = false;
+	}
+	if (HasAuthority())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CountRed: %d"), CountRed);
+		UE_LOG(LogTemp, Warning, TEXT("CountGreen: %d"), CountGreen);
+		UE_LOG(LogTemp, Warning, TEXT("CountBlue: %d"), CountBlue);
+	}
+
+	if (CountRed > 0)
+	{
+		Color_on(EColor::Red);
+		//UE_LOG(LogTemp, Warning, TEXT("RED"));
+	}
+	else
+	{
+		Color_off(EColor::Red);
+		//UE_LOG(LogTemp, Warning, TEXT("NORED"));
+	}
+	if (CountGreen > 0)
+	{
+		Color_on(EColor::Green);
+		//UE_LOG(LogTemp, Warning, TEXT("GREEN"));
+	}
+	else
+	{
+		Color_off(EColor::Green);
+		//UE_LOG(LogTemp, Warning, TEXT("NOGREEN"));
+	}
+	if (CountBlue > 0)
+	{
+		Color_on(EColor::Blue);
+		//UE_LOG(LogTemp, Warning, TEXT("BLUE"));
+	}
+	else
+	{
+		Color_off(EColor::Blue);
+		//UE_LOG(LogTemp, Warning, TEXT("NOBLUE"));
+	}
+	SetLightColor(color);
 }
 
 void ALamp::Toggle_Lamp()
 {
-	if(LightSource->Intensity == 0)
+	if (LightSource->Intensity == 0)
 		LightSource->SetIntensity(5000);
 	else
 		LightSource->SetIntensity(0);
 }
 void ALamp::Color_on(EColor Color)
 {
-	FLinearColor color = LightSource->GetLightColor();
 	if (Player)
 	{
 		switch (Color)
 		{
-		case White:
+		case EColor::White:
 		{
 			color.R = color.G = color.B = 1;
 			break;
 		}
-		case Red:
+		case EColor::Red:
 		{
 			color.R = 1;
 			break;
 		}
-		case Green:
+		case EColor::Green:
 		{
 			color.G = 1;
 			break;
 		}
-		case Blue:
+		case EColor::Blue:
 		{
 			color.B = 1;
 			break;
 		}
 		default: {}
 		}
-		LightSource->SetLightColor(color, true);
-	}	
+	}
 }
-
 void ALamp::Color_off(EColor Color)
 {
-	FLinearColor color = LightSource->GetLightColor();
 	if (Player)
 	{
 		switch (Color)
 		{
-		case Red:
+		case EColor::Red:
 		{
 			color.R = 0;
 			break;
 		}
-		case Green:
+		case EColor::Green:
 		{
 			color.G = 0;
 			break;
 		}
-		case Blue:
+		case EColor::Blue:
 		{
 			color.B = 0;
 			break;
 		}
 		default: {}
 		}
-		LightSource->SetLightColor(color, true);
 	}
+
 }
 
