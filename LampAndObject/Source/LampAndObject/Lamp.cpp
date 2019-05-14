c1c42d26bacc6ea26674de8b2ecad2a86e81d064
MichalNowak98 <48417024+MichalNowak98@users.noreply.github.com>
2019-05-14 17:48:48 +0200

    Cleared class Lamp, rebuilding data flow

--- LampAndObject/Source/LampAndObject/Lamp.cpp
+++ LampAndObject/Source/LampAndObject/Lamp.cpp
@@ -1,360 +1,59 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Lamp.h"
 
-
-ALamp::ALamp(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+ALamp::ALamp(const FObjectInitializer& ObjectInitializer) :Super(ObjectInitializer)
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	CountRed = 0;
-	CountGreen = 0;
-	CountBlue = 0;
-	TriggerVolumeRedPushed = false;
-	TriggerVolumeGreenPushed = false;
-	TriggerVolumeBluePushed = false;
-
-	LightSource = CreateDefaultSubobject<UPointLightComponent>(TEXT("LightSource"));
-	SetReplicates(true);
+	//replication flags
 	bReplicates = true;
 	bNetLoadOnClient = true;
 	bAlwaysRelevant = true;
 
-	if (GetWorld())
-		PlayerController = Cast<AMyPlayerController>(GetWorld()->GetFirstPlayerController());
-
-	//if creating of UPointLightComponent was successful
+	LightSource = CreateDefaultSubobject<UPointLightComponent>(TEXT("LightSource"));
 	if (LightSource)
 	{
 		LightSource->SetIntensity(5000);
-		color = LightSource->GetLightColor();
 	}
 }
 
-void ALamp::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
-{
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-	DOREPLIFETIME(ALamp, color);
-	DOREPLIFETIME(ALamp, CountRed);
-	DOREPLIFETIME(ALamp, CountGreen);
-	DOREPLIFETIME(ALamp, CountBlue);
-}
-
-void ALamp::SetLightColor_Implementation(const FLinearColor Color)
-{
-	//color = Color;
-	if (HasAuthority())
-	{
-		if (color.R == 1)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ServerRED"));
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ServerNoRED"));
-		}
-		if (color.B == 1)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ServerBlue"));
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ServerNoBlue"));
-		}
-		if (color.G == 1)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ServerGREEN"));
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ServerNoGREEN"));
-		}
-	}
-	else
-	{
-		if (color.R == 1)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ClientRED"));
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ClientNoRED"));
-		}
-		if (color.B == 1)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ClientBlue"));
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ClientNoBlue"));
-		}
-		if (color.G == 1)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ClientGREEN"));
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("ClientNoGREEN"));
-		}
-	}
-	LightSource->SetLightColor(color, true);
-}
-
-void ALamp::increment_CountRed_Implementation()
-{
-	CountRed++;
-	UE_LOG(LogTemp, Warning, TEXT("Client cos robi!"));
-}
-
-bool ALamp::increment_CountRed_Validate()
-{
-	return true;
-}
-
-void ALamp::increment_CountGreen_Implementation()
-{
-	CountGreen++;
-}
-
-bool ALamp::increment_CountGreen_Validate()
-{
-	return true;
-}
-
-void ALamp::increment_CountBlue_Implementation()
-{
-	CountBlue++;
-}
-
-bool ALamp::increment_CountBlue_Validate()
-{
-	return true;
-}
-
-void ALamp::decrement_CountRed_Implementation()
-{
-	CountRed--;
-}
-
-bool ALamp::decrement_CountRed_Validate()
-{
-	return true;
-}
-
-void ALamp::decrement_CountGreen_Implementation()
-{
-	CountGreen--;
-}
-
-bool ALamp::decrement_CountGreen_Validate()
-{
-	return true;
-}
-
-void ALamp::decrement_CountBlue_Implementation()
-{
-	CountBlue--;
-}
-
-bool ALamp::decrement_CountBlue_Validate()
-{
-	return true;
-}
-
+// Called when the game starts or when spawned
 void ALamp::BeginPlay()
 {
 	Super::BeginPlay();
 	Player = GetWorld()->GetFirstPlayerController()->GetPawn();
+	PlayerController = Cast<AMyPlayerController>(GetWorld()->GetFirstPlayerController());
 }
 
+// Called every frame
 void ALamp::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 	if (TriggerVolumeRed && TriggerVolumeRed->IsOverlappingActor(Player))
 	{
-		if (!TriggerVolumeRedPushed)
-		{
-			increment_CountRed();
-			if (!HasAuthority())
-			{
-				PlayerController->Server_Set_CountRed(CountRed);
-			}
-		}
-		TriggerVolumeRedPushed = true;
+
 	}
 	else
 	{
-		if (TriggerVolumeRedPushed)
-		{
-			decrement_CountRed();
-			if (!HasAuthority())
-			{
-				PlayerController->Server_Set_CountRed(CountRed);
-			}
-		}
-		TriggerVolumeRedPushed = false;
+
 	}
 	if (TriggerVolumeGreen && TriggerVolumeGreen->IsOverlappingActor(Player))
 	{
-		if (!TriggerVolumeGreenPushed)
-		{
-			increment_CountGreen();
-			if (!HasAuthority())
-			{
-				PlayerController->Server_Set_CountGreen(CountGreen);
-			}
-		}
-			
-		TriggerVolumeGreenPushed = true;
+
 	}
 	else
 	{
-		if (TriggerVolumeGreenPushed)
-		{
-			decrement_CountGreen();
-			if (!HasAuthority())
-			{
-				PlayerController->Server_Set_CountGreen(CountGreen);
-			}
-		}
-			
-		TriggerVolumeGreenPushed = false;
+
 	}
 	if (TriggerVolumeBlue && TriggerVolumeBlue->IsOverlappingActor(Player))
 	{
-		if (!TriggerVolumeBluePushed)
-		{
-			increment_CountBlue();
-			if (!HasAuthority())
-			{
-				PlayerController->Server_Set_CountBlue(CountBlue);
-			}
-		}
-			
-		TriggerVolumeBluePushed = true;
-	}
-	else
-	{
-		if (TriggerVolumeBluePushed)
-		{
-			decrement_CountBlue();
-			if (!HasAuthority())
-			{
-				PlayerController->Server_Set_CountBlue(CountBlue);
-			}
-		}
-			
-		TriggerVolumeBluePushed = false;
-	}
-	if (HasAuthority())
-	{
-		UE_LOG(LogTemp, Warning, TEXT("CountRed: %d"), CountRed);
-		UE_LOG(LogTemp, Warning, TEXT("CountGreen: %d"), CountGreen);
-		UE_LOG(LogTemp, Warning, TEXT("CountBlue: %d"), CountBlue);
-	}
 
-	if (HasAuthority())
-	{
-		CountRed = AMyPlayerController::Server_Get_CountRed();
-		CountGreen = AMyPlayerController::Server_Get_CountGreen();
-		CountBlue = AMyPlayerController::Server_Get_CountBlue();
-	}
-	if (CountRed > 0)
-	{
-		Color_on(EColor::Red);
-		//UE_LOG(LogTemp, Warning, TEXT("RED"));
 	}
 	else
 	{
-		Color_off(EColor::Red);
-		//UE_LOG(LogTemp, Warning, TEXT("NORED"));
+
 	}
-	if (CountGreen > 0)
-	{
-		Color_on(EColor::Green);
-		//UE_LOG(LogTemp, Warning, TEXT("GREEN"));
-	}
-	else
-	{
-		Color_off(EColor::Green);
-		//UE_LOG(LogTemp, Warning, TEXT("NOGREEN"));
-	}
-	if (CountBlue > 0)
-	{
-		Color_on(EColor::Blue);
-		//UE_LOG(LogTemp, Warning, TEXT("BLUE"));
-	}
-	else
-	{
-		Color_off(EColor::Blue);
-		//UE_LOG(LogTemp, Warning, TEXT("NOBLUE"));
-	}
-	SetLightColor(color);
 }
 
-void ALamp::Toggle_Lamp()
-{
-	if (LightSource->Intensity == 0)
-		LightSource->SetIntensity(5000);
-	else
-		LightSource->SetIntensity(0);
-}
-void ALamp::Color_on(EColor Color)
-{
-	if (Player)
-	{
-		switch (Color)
-		{
-		case EColor::White:
-		{
-			color.R = color.G = color.B = 1;
-			break;
-		}
-		case EColor::Red:
-		{
-			color.R = 1;
-			break;
-		}
-		case EColor::Green:
-		{
-			color.G = 1;
-			break;
-		}
-		case EColor::Blue:
-		{
-			color.B = 1;
-			break;
-		}
-		default: {}
-		}
-	}
-}
-void ALamp::Color_off(EColor Color)
-{
-	if (Player)
-	{
-		switch (Color)
-		{
-		case EColor::Red:
-		{
-			color.R = 0;
-			break;
-		}
-		case EColor::Green:
-		{
-			color.G = 0;
-			break;
-		}
-		case EColor::Blue:
-		{
-			color.B = 0;
-			break;
-		}
-		default: {}
-		}
-	}
-
-}
 
--- LampAndObject/Source/LampAndObject/Lamp.h
+++ LampAndObject/Source/LampAndObject/Lamp.h
@@ -3,95 +3,35 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Runtime/Engine/Classes/Components/PointLightComponent.h"
-#include "Engine/TriggerVolume.h"
-#include "Runtime/Core/Public/Math/Color.h"
-#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
-#include "MyPlayerController.h"
-#include "Engine/World.h"
-#include "UnrealNetwork.h"
 #include "GameFramework/Actor.h"
+#include "Engine/TriggerVolume.h"
+#include "MyPlayerController.h"
+#include "Runtime/Engine/Classes/Components/PointLightComponent.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
 #include "Lamp.generated.h"
-
-UENUM()
-enum class EColor : uint8
-{
-	White UMETA(DisplayName = "White"),
-	Red UMETA(DisplayName = "Red"),
-	Green UMETA(DisplayName = "Green"),
-	Blue UMETA(DisplayName = "Blue")
-};
 
 UCLASS()
 class LAMPANDOBJECT_API ALamp : public AActor
 {
 	GENERATED_BODY()
-
+	
 public:
 	ALamp(const FObjectInitializer& ObjectInitializer);
 	virtual void Tick(float DeltaTime) override;
-	//turns lamp on and off
-	void Toggle_Lamp();
 
-	//sets values of given channels to 1 keeping other values unchanged (mixed colors possible).
-	//R = G = B = 1 for "White"
-	void Color_on(EColor Color);
-	//sets values of given channels to 0
-	void Color_off(EColor Color);
-
-	UFUNCTION(NetMulticast, Reliable)
-		void SetLightColor(const FLinearColor Color);
-	void SetLightColor_Implementation(const FLinearColor Color);
-
-	UFUNCTION(Server, Reliable, WithValidation)
-		void increment_CountRed();
-	void increment_CountRed_Implementation();
-	bool increment_CountRed_Validate();
-	UFUNCTION(Server, Reliable, WithValidation)
-		void increment_CountGreen();
-	void increment_CountGreen_Implementation();
-	bool increment_CountGreen_Validate();
-	UFUNCTION(Server, Reliable, WithValidation)
-		void increment_CountBlue();
-	void increment_CountBlue_Implementation();
-	bool increment_CountBlue_Validate();
-	UFUNCTION(Server, Reliable, WithValidation)
-		void decrement_CountRed();
-	void decrement_CountRed_Implementation();
-	bool decrement_CountRed_Validate();
-	UFUNCTION(Server, Reliable, WithValidation)
-		void decrement_CountGreen();
-	void decrement_CountGreen_Implementation();
-	bool decrement_CountGreen_Validate();
-	UFUNCTION(Server, Reliable, WithValidation)
-		void decrement_CountBlue();
-	void decrement_CountBlue_Implementation();
-	bool decrement_CountBlue_Validate();
 protected:
+	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 private:
+	AActor* Player;
 	AMyPlayerController* PlayerController;
-	bool TriggerVolumeRedPushed;
-	bool TriggerVolumeGreenPushed;
-	bool TriggerVolumeBluePushed;
-	UPROPERTY(Replicated)
-		FLinearColor color;
-	UPROPERTY(Replicated)
-		int CountRed;
-	UPROPERTY(Replicated)
-		int CountGreen;
-	UPROPERTY(Replicated)
-		int CountBlue;
-	class UPointLightComponent *LightSource;
-	AActor *Player;
-
-	//pointers to trigger volumes from level
+	class UPointLightComponent* LightSource;
 	UPROPERTY(EditAnywhere)
-		ATriggerVolume *TriggerVolumeRed;
+		ATriggerVolume* TriggerVolumeRed;
 	UPROPERTY(EditAnywhere)
-		ATriggerVolume *TriggerVolumeGreen;
+		ATriggerVolume* TriggerVolumeGreen;
 	UPROPERTY(EditAnywhere)
-		ATriggerVolume *TriggerVolumeBlue;
+		ATriggerVolume* TriggerVolumeBlue;
 
 };
