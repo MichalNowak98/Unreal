14a6c549cf4a932c1edd328bd830ad2cc58041d6
MichalNowak98 <48417024+MichalNowak98@users.noreply.github.com>
2019-05-16 18:44:54 +0200

    Initial commint of version with delegates (no replication)

---
+++ Lamp.cpp
@@ -0,0 +1,165 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Lamp.h"
+
+ALamp::ALamp(const FObjectInitializer& ObjectInitializer) :Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	//replication flags
+	bReplicates = true;
+	bNetLoadOnClient = true;
+	bAlwaysRelevant = true;
+
+	LightSource = CreateDefaultSubobject<UPointLightComponent>(TEXT("LightSource"));
+	TriggerVolumeRed = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Red"));
+	TriggerVolumeGreen = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Green"));
+	TriggerVolumeBlue =  CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Blue"));
+
+	Color.R = Color.G = Color.B = 0;
+
+	if (LightSource)
+	{
+		LightSource->SetIntensity(5000);
+		LightSource->SetLightColor(FLinearColor(0, 0, 0, 1), true);
+	}
+}
+
+void ALamp::TriggerRed(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (OtherActor == Player)
+	{
+		Color_on(EColor::Red);
+	}
+	SetLightColor_Implementation(Color);
+
+}
+
+void ALamp::TriggerGreen(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (OtherActor == Player)
+	{
+		Color_on(EColor::Green);
+	}
+	SetLightColor_Implementation(Color);
+}
+
+void ALamp::TriggerBlue(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (OtherActor == Player)
+	{
+		Color_on(EColor::Blue);
+	}
+	SetLightColor_Implementation(Color);
+}
+
+void ALamp::TriggerRedEnd(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
+	{
+		Color_off(EColor::Red);
+		SetLightColor_Implementation(Color);
+	}
+}
+
+void ALamp::TriggerGreenEnd(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
+	{
+		Color_off(EColor::Green);
+		SetLightColor_Implementation(Color);
+	}
+}
+
+void ALamp::TriggerBlueEnd(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
+	{
+		Color_off(EColor::Blue);
+		SetLightColor_Implementation(Color);
+	}
+}
+
+// Called when the game starts or when spawned
+void ALamp::BeginPlay()
+{
+	Super::BeginPlay();
+	Player = GetWorld()->GetFirstPlayerController()->GetPawn();
+	PlayerController = Cast<AMyPlayerController>(GetWorld()->GetFirstPlayerController());
+	TriggerVolumeRed->OnComponentBeginOverlap.AddDynamic(this, &ALamp::TriggerRed);
+	TriggerVolumeGreen->OnComponentBeginOverlap.AddDynamic(this, &ALamp::TriggerGreen);
+	TriggerVolumeBlue->OnComponentBeginOverlap.AddDynamic(this, &ALamp::TriggerBlue);
+	TriggerVolumeRed->OnComponentEndOverlap.AddDynamic(this, &ALamp::TriggerRedEnd);
+	TriggerVolumeGreen->OnComponentEndOverlap.AddDynamic(this, &ALamp::TriggerGreenEnd);
+	TriggerVolumeBlue->OnComponentEndOverlap.AddDynamic(this, &ALamp::TriggerBlueEnd);
+}
+
+// Called every frame
+void ALamp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+}
+
+void ALamp::Color_on(EColor Ecolor)
+{
+	if (Player)
+	{
+		switch (Ecolor)
+		{
+		case EColor::White:
+		{
+			Color.R = Color.G = Color.B = 1;
+			break;
+		}
+		case EColor::Red:
+		{
+			Color.R = 1;
+			break;
+		}
+		case EColor::Green:
+		{
+			Color.G = 1;
+			break;
+		}
+		case EColor::Blue:
+		{
+			Color.B = 1;
+			break;
+		}
+		default: {}
+		}
+	}
+}
+
+void ALamp::Color_off(EColor Ecolor)
+{
+	if (Player)
+	{
+		switch (Ecolor)
+		{
+		case EColor::Red:
+		{
+			Color.R = 0;
+			break;
+		}
+		case EColor::Green:
+		{
+			Color.G = 0;
+			break;
+		}
+		case EColor::Blue:
+		{
+			Color.B = 0;
+			break;
+		}
+		default: {}
+		}
+	}
+
+}
+
+void ALamp::SetLightColor_Implementation(const FLinearColor & Color)
+{
+	LightSource->SetLightColor(Color);
+}
---
+++ Lamp.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/TriggerVolume.h"
+#include "MyPlayerController.h"
+#include "Components/BoxComponent.h"
+#include "Runtime/Engine/Classes/Components/PointLightComponent.h"
+#include "Runtime/Core/Public/Math/Color.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+#include "Lamp.generated.h"
+
+UENUM()
+enum class EColor
+{
+	White UMETA(DisplayName = "White"),
+	Red UMETA(DisplayName = "Red"),
+	Green UMETA(DisplayName = "Green"),
+	Blue UMETA(DisplayName = "Blue")
+};
+
+UCLASS()
+class LAMPANDOBJECT_API ALamp : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ALamp(const FObjectInitializer& ObjectInitializer);
+	virtual void Tick(float DeltaTime) override;
+
+	void Color_on(EColor Ecolor);
+	void Color_off(EColor Ecolor);
+	UFUNCTION(NetMulticast, Reliable)
+		void SetLightColor(const FLinearColor& Color);
+	void SetLightColor_Implementation(const FLinearColor& Color);
+
+	UPROPERTY(VisibleAnywhere)
+		class UBoxComponent* TriggerVolumeRed;
+	UPROPERTY(VisibleAnywhere)
+		class UBoxComponent* TriggerVolumeGreen;
+	UPROPERTY(VisibleAnywhere)
+		class UBoxComponent* TriggerVolumeBlue;
+	UFUNCTION()
+		void TriggerRed(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+		void TriggerGreen(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+		void TriggerBlue(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+		void TriggerRedEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	UFUNCTION()
+		void TriggerGreenEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	UFUNCTION()
+		void TriggerBlueEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	AActor* Player;
+	AMyPlayerController* PlayerController;
+	class UPointLightComponent* LightSource;
+	FLinearColor Color;
+};
