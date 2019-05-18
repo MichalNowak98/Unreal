0824d5dd548acb4dbaecc4bcb2bef5d488771173
MichalNowak98 <48417024+MichalNowak98@users.noreply.github.com>
2019-05-18 12:32:12 +0200

    DestructibleObject initial commit.

---
+++ LampAndObject/Source/LampAndObject/DestructibleObject.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DestructibleObject.h"
+
+// Sets default values
+ADestructibleObject::ADestructibleObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+	TriggerComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Component"));
+	TriggerComponent->SetupAttachment(RootComponent);
+
+
+	IsDestroyed = false;
+	MaxHealth = 10.f;
+	DefaultDamage = 10.f;
+	DefaultImpulse = 10.f;
+}
+
+// Called when the game starts or when spawned
+void ADestructibleObject::BeginPlay()
+{
+	Super::BeginPlay();
+	if (DestructibleComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("there is something to destroy"));
+		DestructibleComponent->SetNotifyRigidBodyCollision(true);
+	}
+	TriggerComponent->OnComponentBeginOverlap.AddDynamic(this, &ADestructibleObject::Trigger);
+	CurrentHealth = MaxHealth;
+}
+
+// Called every frame
+void ADestructibleObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Player = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void ADestructibleObject::Trigger(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!IsDestroyed && DestructibleComponent && OtherActor == Player)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Trigger worked"));
+		Destroy(DefaultDamage, DestructibleComponent->GetComponentLocation(), OtherActor->GetActorForwardVector(), DefaultImpulse);
+	}
+}
+
+void ADestructibleObject::Destroy(float Damage, FVector HitLocation, FVector ImpulseDir, float Impulse)
+{
+	if (!IsDestroyed)
+	{
+		IsDestroyed = true;
+		UE_LOG(LogTemp, Warning, TEXT("Destruction worked"));
+		DestructibleComponent->ApplyDamage(Damage, HitLocation, ImpulseDir, Impulse);
+	}
+}
---
+++ LampAndObject/Source/LampAndObject/DestructibleObject.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DestructibleComponent.h"
+#include "Runtime/Engine/Classes/Components/BoxComponent.h"
+#include "Engine/World.h"
+#include "DestructibleObject.generated.h"
+
+UCLASS()
+class LAMPANDOBJECT_API ADestructibleObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ADestructibleObject();
+	virtual void Tick(float DeltaTime) override;
+	UFUNCTION()
+		void Trigger(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+		void Destroy(float Damage, FVector HitLocation, FVector ImpulseDir, float Impulse);
+	UPROPERTY(EditAnywhere)
+		class UDestructibleComponent* DestructibleComponent;
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+		class UBoxComponent* TriggerComponent;
+
+	AActor* Player;
+	UPROPERTY(VisibleAnywhere)
+		bool IsDestroyed;
+	UPROPERTY(VisibleAnywhere)
+		float MaxHealth;
+	UPROPERTY(VisibleAnywhere)
+		float CurrentHealth;
+	UPROPERTY(VisibleAnywhere)
+		float DefaultDamage;
+	UPROPERTY(VisibleAnywhere)
+		float DefaultImpulse;
+};
